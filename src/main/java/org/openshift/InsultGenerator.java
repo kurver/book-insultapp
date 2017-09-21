36434b1966c0d76a3771b6608ebe7c3acb5f3099
Kerry Burke <kerry@divisibleprime.com>
2017-09-21 18:50:14 +1200

    database changes

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,19 +1,46 @@
 package org.openshift;
 
-import java.util.Random;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class InsultGenerator {
 	public String generateInsult() {
-		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
+		
 		String vowels = "AEIOU";
 		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
-		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+                String theInsult = "";
+
+                try {
+			String databaseURL = "jdbc:postgresql://";
+			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+
+			String username = System.getenv("POSTGRESQL_USER");
+			String password = System.getenv("PGPASSWORD");
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
+
+			if(connection != null) {
+			    String sql = "SELECT A.STRING AS FIRST, B.STRING AS SECOND, C.STRING AS NOUN FROM SHORT_ADJECTIVE A, LONG_ADJECTIVE B, NOUN C ORDER BY RANDOM() LIMIT 1";
+
+			    Statement stmt = connection.createStatement();
+			    ResultSet rs = stmt.executeQuery(sql);
+			    while(rs.next()) {
+				if(vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+				    article = "a";
+				}
+				theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("FIRST"), rs.getString("SECOND"), rs.getString("NOUN"));
+			    }
+			    rs.close();
+			    connection.close();
+			}
+	        } catch( Exception e) {
+	            return "Database connection problem";
+	        }
+                        
+
+		return theInsult;
 	}
 
 }
